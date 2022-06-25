c67ba0a608c00cabac8759fb0d4742d01ca6e39a
patterson9000 <106724462+patterson9000@users.noreply.github.com>
2022-06-24 22:36:03 -0700

    Update and rename README.md to script3.js

---
+++ script3.js
@@ -0,0 +1,12 @@
+let name=prompt("product name");
+let price=prompt("product price");
+let qty=prompt("product qty");
+let subtotal=(price* qty);
+let tax= 1.03;
+let total= (subtotal*tax);
+console.log("product="+ name);
+console.log("price="+ price);
+console.log("qty="+ qty);
+console.log("subtotal="+ subtotal);
+console.log("tax="+ tax);
+console.log("total="+ total);
